0c7c8f8174a2676944ffe6dc56084f50c9704db8
Arpit Jain <arpitjaingwl7@gmail.com>
2025-10-31 01:11:39 +0530

    database configuration

---
+++ Backend-Service/src/models/user.js
@@ -0,0 +1,32 @@
+
+const mongoose = require("mongoose")
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+    }
+    ,
+    designation: {
+        type: String,
+    }
+
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = {
+    User,
+}
+
+
