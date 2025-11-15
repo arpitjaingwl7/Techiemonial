7572736d218a2f5ac92a0aff5eca2f43f2e2ca1e
Arpit Jain <arpitjaingwl7@gmail.com>
2025-11-15 23:42:15 +0530

    Feed API

--- Backend-Service/src/routes/userRoutes.js
+++ Backend-Service/src/routes/userRoutes.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const { isUserValid } = require("../middleware/auth.middleware");
 const { ConnectionRequest } = require("../models/connectionRequest");
+const {User}=require("../models/user")
 
 const userRouter = express.Router();
 
@@ -72,8 +73,8 @@ userRouter.get( "/user/request/connections",isUserValid,async(req,res)=>{
           .populate("fromUserId",safe_User_Data)
 
           const data = connectionRequests.map((key)=>{
-              console.log(key.fromUserId)
-              console.log(loggedInUser._id)
+            //   console.log(key.fromUserId)
+            //   console.log(loggedInUser._id)
             if(key.fromUserId.equals(loggedInUser._id)){
                 return key.toUserId
             }
@@ -98,6 +99,51 @@ userRouter.get( "/user/request/connections",isUserValid,async(req,res)=>{
 })
 
 // APi for getting feed
+
+
+userRouter.get("/user/feed",isUserValid,async(req,res)=>{
+
+
+    // feed should not consist of user self and any other excisting connectioRequest with loggedin user
+    // pagination
+
+
+    try {
+        const loggedInUser=req.user
+
+        const page=req.query.page||1
+        let limit=req.query.limit||10
+
+        if(limit>5){
+            limit=5
+        }
+
+
+        const skip=(page-1)*limit
+
+        const connectionRequests=await ConnectionRequest.find({
+            $or:[{fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}]
+        })
+        const hiddenUser=new Set()
+
+        connectionRequests.forEach(cr => {
+            hiddenUser.add(cr.toUserId)
+            hiddenUser.add(cr.fromUserId)
+        });
+
+        const feedUserData=await User.find({_id:{$nin:Array.from(hiddenUser)}}).select(safe_User_Data).skip(skip).limit(limit)
+
+     res.status(200).json({
+        message:"data fetched successfuly",
+        data:feedUserData
+     })
+
+
+    } catch (error) {
+
+        res.status(401).json({message:error.message})   
+    }
+})
 
 
 module.exports = {userRouter}
